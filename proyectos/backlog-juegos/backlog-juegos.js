6440f9269b044fdc72e413449841687075adf97a
facuvazquez1 <facugaston98@gmail.com>
2026-08-15 20:11:09 -0300

    Agregar datos de ejemplo al backlog de videojuegos

---
+++ proyectos/backlog-juegos/backlog-juegos.js
@@ -0,0 +1,53 @@
+const listaJuegos = [
+
+    {
+    id: 1,         
+    titulo: "World of Warcraft",     
+    plataforma: "PC",  
+    genero: "MMORPG",   
+    horas: 1200,      
+    completado: false, 
+    puntaje: 10,      
+    },
+
+    {
+    id: 2,         
+    titulo: "League of Legends",     
+    plataforma: "PC",  
+    genero: "MOBA",   
+    horas: 4000,      
+    completado: false, 
+    puntaje: 8,      
+    },
+
+    {
+    id: 3,         
+    titulo: "FIFA 2027",     
+    plataforma: "PS5",  
+    genero: "Deporte",   
+    horas: 700,      
+    completado: true, 
+    puntaje: 6,      
+    },
+
+    {
+    id: 4,         
+    titulo: "Call of Duty 3",     
+    plataforma: "XBOX 360",  
+    genero: "Shooter",   
+    horas: 400,      
+    completado: true, 
+    puntaje: 4,      
+    },
+
+    {
+    id: 5,         
+    titulo: "Pokemon Esmeralda",     
+    plataforma: "GAMEBOY ADVANCE",  
+    genero: "RPG",   
+    horas: 120,      
+    completado: false, 
+    puntaje: 3,      
+    },
+
+]
