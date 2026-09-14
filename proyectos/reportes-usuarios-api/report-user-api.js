edec982575447bfccc85efdbb13e464d90147b68
facuvazquez1 <facugaston98@gmail.com>
2026-09-14 18:49:31 -0300

    Finalizamos proyecto report-user-api

--- proyectos/reportes-usuarios-api/report-user-api.js
+++ proyectos/reportes-usuarios-api/report-user-api.js
@@ -55,20 +55,81 @@ async function generarReporteUsuario(id) {
 
 }
 
-console.log(await generarReporteUsuario(1))
+// console.log(await generarReporteUsuario(1))
 
 // generarReportesDeUsuarios: recibe un array de ids, arma los reportes
 // en paralelo con Promise.all.
-async function generarReportesDeUsuarios(ids) {}
+async function generarReportesDeUsuarios(ids) {
+
+    const reportes = ids.map(id => generarReporteUsuario(id))
+
+    const datos = await Promise.all(reportes)
+
+    return datos
+    
+
+}
+
+// console.log( await generarReportesDeUsuarios([1, 2, 3]))
 
 // generarReportesConTolerancia: igual que la anterior pero con
 // Promise.allSettled. Devuelve { exitosos, fallidos }.
-async function generarReportesConTolerancia(ids) {}
+async function generarReportesConTolerancia(ids) {
+
+    const reportes = ids.map(id => generarReporteUsuario(id))
+
+    const datos = await Promise.allSettled(reportes)
+    
+    const exitosos = datos.filter(reporte => reporte.status === "fulfilled").map(report => report.value)
+
+    const fallidos = datos.filter(reporte => reporte.status === "rejected").map(report => report.reason)
+
+    return {exitosos, fallidos}
+
+}
+
+// console.log(await generarReportesConTolerancia([1, 2, 9999])) 
+
 
 // crearPost: recibe userId, titulo y body. Hace POST a /posts.
 // Devuelve el objeto creado.
-async function crearPost(userId, titulo, body) {}
+async function crearPost(userId, titulo, body) {
+    
+    const newPost = {
+        userId: userId,
+        title: titulo,
+        body: body
+    }
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newPost)
+    })
+
+    if(!response.ok){
+        throw new Error(`STATUS ERROR ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    return data
+
+}
+
+// console.log(await crearPost(1, "titulo", "body"))
 
 // agregarPostYReporte: crea un post con crearPost y devuelve el
 // reporte actualizado del usuario.
-async function agregarPostYReporte(id, titulo, body) {}
+async function agregarPostYReporte(id, titulo, body) {
+
+    const newPost = await crearPost(id, titulo, body)
+    const newReport = await generarReporteUsuario(id)
+
+    return newReport
+}
+
+console.log(await agregarPostYReporte(2, "titulo22", "body22"))
+
